47d82f263c2c564668fb25231384cdd27f8cb771
Ksenia <Sheggyshelti@mail.ru>
2022-02-05 14:53:45 +0300

    Готовая домашка №3

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Задание №1");
+        int a = 0;
+        while (a<10) {
+            a=a+1;
+            System.out.print(a + " ");
+        }
+        System.out.println(" ");
+        for (int b = 10;b >= 1; b=b-1) {
+
+            System.out.print( b +" ");
+        }
+
+        System.out.println("Задача №2");
+
+        int friday = 4;
+        while (friday <= 31 ) {
+            s = friday + 7;
+            System.out.println(s);
+        }
     }
 }
