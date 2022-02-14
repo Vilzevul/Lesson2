07d5398970be59fb500383b973be5b343091566f
Ksenia <Sheggyshelti@mail.ru>
2022-02-14 20:06:38 +0300

    Строки, поправить 3 задание

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,29 @@
 package ru.skypro;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Задание №1
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+
+        //Задание №2
+        String fullName2 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName2);
+
+        //Задание №3
+        String[] s = fullName.split(" ; ");
+        System.out.println("Данные ФИО сотрудника для административного отдела - " + s);
+
+        //Задание №4
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + fullName);
+
+
     }
 }
