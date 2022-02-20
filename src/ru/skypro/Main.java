b88e6665483171d5a744a9c41cfd8b6ae39398c1
Ksenia <Sheggyshelti@mail.ru>
2022-02-20 10:02:17 +0300

    ДЗ по методам

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -70,7 +70,7 @@ public class Main {
         deliveryTime(45);
         stringDuble("abbcdde");
 
-        int[] arr = {1, 2, 3, 4, 5,6,7,8,9,10};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         massiv(arr);
 
 
